1f7ee1daf1457de42f15257b44045c10d0eb0b45
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 17:27:06 -0500

    create new file using Python's 'write' ability

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 with open('my_file.txt', mode='w') as file:
     file.write('I am writing this into the file using Python')
 
-
+with open('new_file.txt', mode='w') as file:
+    file.write('I\'ve created this file using Python')
