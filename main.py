70e56ea834058722bc3dffe6c8f5a51fd2417a3c
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 19:43:44 -0500

    playing around with absolute and relative file paths

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-with open('my_file.txt', mode='w') as file:
-    file.write('I am writing this into the file using Python')
+with open('../../my_file.txt', mode='w') as file:
+    file.write('I am trying to teach myself Python')
+
+with (open('../../my_file.txt', mode='r')) as file:
+    contents = file.read()
+    print(contents)
 
 with open('new_file.txt', mode='w') as file:
     file.write('I\'ve created this file using Python')
+
